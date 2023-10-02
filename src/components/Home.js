50947458deb43788468867758c66f5307aa18469
Siri Chandana Vemaraju <svemaraju@seattleu.edu>
2023-10-02 13:13:56 -0700

    created button to call

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home flex flex-col items-center">
+      <button className="bg-black text-white rounded-sm p-5 m-6">Call</button>
+    </div>
+  );
+};
+
+export default Home;
